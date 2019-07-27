60de995e0a62c80883a5d7bac5d198b897eb0ae4
Juan Valdes <mw3_america@hotmail.com>
2019-07-27 18:02:25 -0500

    Initial commit

---
+++ Assets/Scripts/MOVGEMI.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MOVGEMI : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public float velocidad=5;
+    public float salto = 3;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.UpArrow))
+        {
+            transform.Translate(Vector3.forward * velocidad * Time.deltaTime);
+
+        }
+
+        if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            transform.Translate(Vector3.left * velocidad * Time.deltaTime);
+        }
+        if (Input.GetKey(KeyCode.DownArrow))
+        {
+            transform.Translate(Vector3.back * velocidad * Time.deltaTime);
+        }
+        if (Input.GetKey(KeyCode.RightArrow))
+        {
+            transform.Translate(Vector3.right * velocidad * Time.deltaTime);
+        }
+
+        if (Input.GetKey(KeyCode.Space))
+        {
+            transform.Translate(Vector3.up * salto * Time.deltaTime);
+        }
+
+    }
+}
